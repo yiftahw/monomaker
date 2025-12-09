da115dc4c1e0bc235b991219945d97119bb2b7cb
Yiftah Waisman <63462505+yiftahw@users.noreply.github.com>
2025-12-09 16:46:40 +0200

    fix CI (#1)

---
+++ get-git-filter-repo.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# get git-filter-repo if not already present from it's github repo
+# https://raw.githubusercontent.com/newren/git-filter-repo/main/git-filter-repo
+curl -o git-filter-repo https://raw.githubusercontent.com/newren/git-filter-repo/main/git-filter-repo
+chmod +x git-filter-repo
